10a7e21ff95352cc2df96389a2e0a81bca4fec61
Bryan Miller <gratiafide@yahoo.com>
2015-12-29 21:56:43 -0600

    added a button to remove completed tasks

--- list_app.js
+++ list_app.js
@@ -5,7 +5,31 @@
 Vue.component('my-task-component',
     {
         template: '#tasks-template',
-        props:['list']
+        props:['list'],
+        computed:{
+            remaining: function(){
+                var vm = this;
+                return tasksLeft = this.list.filter(function(task){
+                    // The filter will return the items from the list array
+                    // that return true, which is determined below.
+                    return ! vm.inProgress(task);
+                }).length;
+            }
+        },
+        methods:{
+            isCompleted: function(task){
+                return task.completed;
+            },
+            inProgress: function(task){
+                return ! task.completed;
+            },
+            deleteTask: function(task){
+                this.list.$remove(task);
+            },
+            clearCompleted: function(){
+                this.list = this.list.filter(this.inProgress);
+            }
+        }
     });
 
 new Vue({
