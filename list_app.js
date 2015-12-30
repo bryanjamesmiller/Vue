1e86f04fe4d251b085c2a97fe35d3bcc7f5c396d
Bryan Miller <gratiafide@yahoo.com>
2015-12-29 18:54:20 -0600

    added a new app

---
+++ list_app.js
@@ -0,0 +1,20 @@
+/**
+ * Created by B on 12/29/2015.
+ */
+
+new Vue({
+   el: '#list_app',
+    data:{
+        is_it_done:'cross_off',
+        tasks:[
+            { 'activity':'Go to the store', 'completed':false},
+            { 'activity':'Go to school', 'completed':false},
+            { 'activity':'Go to home', 'completed':true}
+        ]
+    },
+    methods:{
+        toggle_cross_off_class:function(task){
+            task.completed = ! task.completed;
+        }
+    }
+});
