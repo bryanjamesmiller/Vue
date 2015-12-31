92ff902f977245b4d463262650481c34eeca232b
Bryan Miller <gratiafide@yahoo.com>
2015-12-30 19:56:19 -0600

    added conditional statement for ajax to make the right url call

--- jsonApp/public/ajax.js
+++ jsonApp/public/ajax.js
@@ -6,7 +6,12 @@ Vue.component('tasks', {
         };
     },
     created: function(){
-        $.getJSON('http://jsonapp.localhost/pretend-api', function(tasks){
+        if(window.location.href == 'http://localhost:63342/vue2/jsonApp/public/index.php') {
+            $my_api_url = 'http://jsonapp.localhost/pretend-api';
+        } else {
+            $my_api_url = 'http://jsonapp.grat.xyz/pretend-api';
+        }
+        $.getJSON($my_api_url, function(tasks){
             this.list = tasks;
         }.bind(this));
     }
